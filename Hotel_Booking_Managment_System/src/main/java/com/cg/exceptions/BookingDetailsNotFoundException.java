792049b57aff2c26c2b12c86b4ec351c42472c5b
anubhav kumar <anubhavkumarrocky@gmail.com>
2023-01-29 13:48:57 +0530

    BoolingDetails module added

---
+++ Hotel_Booking_Managment_System/src/main/java/com/cg/exceptions/BookingDetailsNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cg.exceptions;
+
+public class BookingDetailsNotFoundException extends  RuntimeException{
+	public BookingDetailsNotFoundException(String message) {
+		super(message);
+		
+	}
+
+}
